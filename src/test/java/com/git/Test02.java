6e335915d6542dc012c85653397dab80a7fed54a
carlxm <605522658@qq.com>
2021-02-25 16:38:28 +0800

    dev分支添加Test02.java文件

---
+++ src/test/java/com/git/Test02.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @Description: TODO
+ * @author: Carl
+ * @date: 2021年02月25日 16:36
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("Test02...");
+    }
+}
