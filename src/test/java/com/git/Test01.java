bb12da2307c94e4a654f53076bd561395a0dc646
carlxm <605522658@qq.com>
2021-02-25 15:49:59 +0800

    添加test01.java

---
+++ src/test/java/com/git/Test01.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @Description: TODO
+ * @author: Carl
+ * @date: 2021年02月25日 15:47
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01...");
+    }
+}
